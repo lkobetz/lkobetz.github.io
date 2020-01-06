cabff8989ca675d9e36737035ccb2a5f7923b4e7
lkobetz <larissa.kobetz@gmail.com>
2020-01-06 11:07:57 -0500

    temporarily removed unfinished contact form

--- src/Contact.js
+++ src/Contact.js
@@ -13,44 +13,45 @@ class Contact extends React.Component {
 
   render() {
     return (
-      <div className="Contact">
-        <form
-          id="contact-form"
-          onSubmit={this.handleSubmit.bind(this)}
-          method="POST"
-        >
-          <div className="form-group">
-            <label htmlFor="name">Name</label>
-            <input
-              type="text"
-              className="form-control"
-              value={this.state.name}
-              onChange={this.onNameChange.bind(this)}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="exampleInputEmail1">Email address</label>
-            <input
-              type="email"
-              className="form-control"
-              aria-describedby="emailHelp"
-              value={this.state.email}
-              onChange={this.onEmailChange.bind(this)}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="message">Message</label>
-            <textarea
-              className="form-control"
-              rows="5"
-              value={this.state.message}
-              onChange={this.onMessageChange.bind(this)}
-            />
-          </div>
-          <button type="submit" className="btn btn-primary">
-            Submit
-          </button>
-        </form>
+      <h2>Contact page coming soon...</h2>
+      // <div className="Contact">
+      //   <form
+      //     id="contact-form"
+      //     onSubmit={this.handleSubmit.bind(this)}
+      //     method="POST"
+      //   >
+      //     <div className="form-group">
+      //       <label htmlFor="name">Name</label>
+      //       <input
+      //         type="text"
+      //         className="form-control"
+      //         value={this.state.name}
+      //         onChange={this.onNameChange.bind(this)}
+      //       />
+      //     </div>
+      //     <div className="form-group">
+      //       <label htmlFor="exampleInputEmail1">Email address</label>
+      //       <input
+      //         type="email"
+      //         className="form-control"
+      //         aria-describedby="emailHelp"
+      //         value={this.state.email}
+      //         onChange={this.onEmailChange.bind(this)}
+      //       />
+      //     </div>
+      //     <div className="form-group">
+      //       <label htmlFor="message">Message</label>
+      //       <textarea
+      //         className="form-control"
+      //         rows="5"
+      //         value={this.state.message}
+      //         onChange={this.onMessageChange.bind(this)}
+      //       />
+      //     </div>
+      //     <button type="submit" className="btn btn-primary">
+      //       Submit
+      //     </button>
+      //   </form>
       </div>
     );
   }
