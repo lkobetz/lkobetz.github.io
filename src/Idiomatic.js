e120bd410a2143fbc0a4090608214c90aba62229
lkobetz <larissa.kobetz@gmail.com>
2020-02-03 18:51:33 -0500

    embedded videos into projects section

--- src/Idiomatic.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Embed } from "semantic-ui-react";
-
-const Idiomatic = () => (
-  <Embed id="388738619" placeholder="./images/logo.svg" source="vimeo" />
-);
-
-export default Idiomatic;
--- src/Projects.js
+++ src/Projects.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import Idiomatic from "./Idiomatic";
+import Embed from "react-embed";
 
 export default props => {
   return (
@@ -9,7 +9,16 @@ export default props => {
         <h2>Projects</h2>
         <div className={"section"}>
           <h5>Grace In Space</h5>
-          <Idiomatic />
+          <Embed
+            url={
+              "https://www.youtube.com/watch?v=Tg7BLim7ues&list=PLx0iOsdUOUmnZXS2H9EDbhYMlmh3onWHs&index=6&t=0s&fbclid=IwAR2kzuxHrUMe7-RLOt7l7hPCf-cFQ_p8tWf3wwxrgjEzDnx_NX33A5o_Fb4"
+            }
+          />
+          <Embed
+            url={"https://www.youtube.com/watch?v=-HUuZn3BKrQ&feature=youtu.be"}
+          />
+          <h5>Idiomatic</h5>
+          <Embed url={"https://youtu.be/nNw_wC0WL-Y"} />
         </div>
       </div>
     </div>
