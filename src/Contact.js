cef8797269a059f22fffc87e46be71dd6bb82516
lkobetz <larissa.kobetz@gmail.com>
2020-02-05 17:32:48 -0500

    fixed navbar spacing in Projects component

--- src/Contact.js
+++ src/Contact.js
@@ -1,5 +1,5 @@
 import React from "react";
-import axios from "axios";
+// import axios from "axios";
 import NavBar from "./NavBar";
 
 class Contact extends React.Component {
@@ -69,36 +69,36 @@ class Contact extends React.Component {
     );
   }
 
-  onNameChange(event) {
-    this.setState({ name: event.target.value });
-  }
+  // onNameChange(event) {
+  //   this.setState({ name: event.target.value });
+  // }
 
-  onEmailChange(event) {
-    this.setState({ email: event.target.value });
-  }
+  // onEmailChange(event) {
+  //   this.setState({ email: event.target.value });
+  // }
 
-  onMessageChange(event) {
-    this.setState({ message: event.target.value });
-  }
+  // onMessageChange(event) {
+  //   this.setState({ message: event.target.value });
+  // }
 
-  async handleSubmit(event) {
-    event.preventDefault();
-    // create another project in express and send this to that server
-    const response = await axios.post(
-      "https://secret-harbor-77463.herokuapp.com/",
-      this.state
-    ); // localhost: http://localhost:8000
-    console.log("response.data from axios", response.data);
-    if (response.data.status === "success") {
-      alert("Message Sent.");
-      this.resetForm();
-    } else if (response.data.status === "fail") {
-      alert("Message failed to send.");
-    }
-  }
-  resetForm() {
-    this.setState({ name: "", email: "", message: "" });
-  }
+  // async handleSubmit(event) {
+  //   event.preventDefault();
+  //   // create another project in express and send this to that server
+  //   const response = await axios.post(
+  //     "https://secret-harbor-77463.herokuapp.com/",
+  //     this.state
+  //   ); // localhost: http://localhost:8000
+  //   console.log("response.data from axios", response.data);
+  //   if (response.data.status === "success") {
+  //     alert("Message Sent.");
+  //     this.resetForm();
+  //   } else if (response.data.status === "fail") {
+  //     alert("Message failed to send.");
+  //   }
+  // }
+  // resetForm() {
+  //   this.setState({ name: "", email: "", message: "" });
+  // }
 }
 
 export default Contact;
--- src/Projects.js
+++ src/Projects.js
@@ -5,45 +5,49 @@ import Embed from "react-embed";
 
 export default props => {
   return (
-    <div className={"Section-header"}>
+    <div>
       <link
         rel="stylesheet"
         type="text/css"
         href="//fonts.googleapis.com/css?family=Nixie+One"
       />
       <NavBar />
-      <div id={"projects"}>
-        <div className={"single_project"}>
-          <h3 className={"project_item"}>Grace In Space</h3>
-          <h5 className={"project_item"}>
-            Grace in Space is a virtual reality mobile game in which players
-            help an elderly alien collect items that flew out of her bag while
-            she was traveling through space on vacation. <br />
+      <div className={"Section-header"}>
+        <div id={"projects"}>
+          <div className={"single_project"}>
+            <h3 className={"project_item"}>Grace In Space</h3>
+            <h5 className={"project_item"}>
+              Grace in Space is a virtual reality mobile game in which players
+              help an elderly alien collect items that flew out of her bag while
+              she was traveling through space on vacation. <br />
+              <br />
+              It was built with JavaScript, React Native, Redux, Viro React,
+              Firebase, Firestore, and Git
+            </h5>
+            <Embed
+              className={"project_item"}
+              url={
+                "https://www.youtube.com/watch?v=Tg7BLim7ues&list=PLx0iOsdUOUmnZXS2H9EDbhYMlmh3onWHs&index=6&t=0s&fbclid=IwAR2kzuxHrUMe7-RLOt7l7hPCf-cFQ_p8tWf3wwxrgjEzDnx_NX33A5o_Fb4"
+              }
+            />
             <br />
-            It was built with JavaScript, React Native, Redux, Viro React,
-            Firebase, Firestore, and Git
-          </h5>
-          <Embed
-            className={"project_item"}
-            url={
-              "https://www.youtube.com/watch?v=Tg7BLim7ues&list=PLx0iOsdUOUmnZXS2H9EDbhYMlmh3onWHs&index=6&t=0s&fbclid=IwAR2kzuxHrUMe7-RLOt7l7hPCf-cFQ_p8tWf3wwxrgjEzDnx_NX33A5o_Fb4"
-            }
-          />
-          <br />
-          <Embed
-            url={"https://www.youtube.com/watch?v=-HUuZn3BKrQ&feature=youtu.be"}
-          />
-        </div>
-        <div className={"single_project"}>
-          <h3 className={"project_item"}>Idiomatic</h3>
-          <h5 className={"project_item"}>
-            Idiomatic is a mobile app that helps English language learners
-            practice their sentence structure and spelling while learning new
-            idioms. <br />
-            <br />
-            It was built with JavaScript, React Native, Node.js, and Git
-          </h5>
-          <Embed url={"https://www.youtube.com/watch?v=nNw_wC0WL-Y"} />
+            <Embed
+              url={
+                "https://www.youtube.com/watch?v=-HUuZn3BKrQ&feature=youtu.be"
+              }
+            />
+          </div>
+          <div className={"single_project"}>
+            <h3 className={"project_item"}>Idiomatic</h3>
+            <h5 className={"project_item"}>
+              Idiomatic is a mobile app that helps English language learners
+              practice their sentence structure and spelling while learning new
+              idioms. <br />
+              <br />
+              It was built with JavaScript, React Native, Node.js, and Git
+            </h5>
+            <Embed url={"https://www.youtube.com/watch?v=nNw_wC0WL-Y"} />
+          </div>
         </div>
       </div>
     </div>
