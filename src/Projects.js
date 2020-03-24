8471e0cf627608ad9e12da71959bd224a933763f
lkobetz <larissa.kobetz@gmail.com>
2020-03-24 19:11:43 -0400

    changed order of projects

--- src/Projects.js
+++ src/Projects.js
@@ -14,44 +14,6 @@ export default props => {
           <div className={"single_project"}>
             <div className={"projects_text"}>
               <h4 className={"project_item"}>
-                Grace In Space
-                <a href="https://github.com/graceinspace/graceInSpace">
-                  <img
-                    className={"github_space"}
-                    src={github}
-                    width={30}
-                    alt={""}
-                  ></img>
-                </a>
-              </h4>
-              <h5 className={"project_description"}>
-                Grace in Space is a virtual reality mobile game in which players
-                help an elderly alien collect items that flew out of her bag
-                while she was traveling through space on vacation. <br />
-                <br />
-                It was built with Node.js, JavaScript, React Native, Redux, Viro
-                React, Firebase, Firestore, and Git
-                <br />
-              </h5>
-            </div>
-            <div className={"demos"}>
-              <Embed
-                className={"project_item"}
-                url={
-                  "https://www.youtube.com/watch?v=Tg7BLim7ues&list=PLx0iOsdUOUmnZXS2H9EDbhYMlmh3onWHs&index=6&t=0s&fbclid=IwAR2kzuxHrUMe7-RLOt7l7hPCf-cFQ_p8tWf3wwxrgjEzDnx_NX33A5o_Fb4"
-                }
-              />
-              <br />
-              <Embed
-                url={
-                  "https://www.youtube.com/watch?v=-HUuZn3BKrQ&feature=youtu.be"
-                }
-              />
-            </div>
-          </div>
-          <div className={"single_project"}>
-            <div className={"projects_text"}>
-              <h4 className={"project_item"}>
                 Idiomatic
                 <a href="https://github.com/lkobetz/stackathon">
                   <img
@@ -108,6 +70,44 @@ export default props => {
             </div>
             <div className={"demos"}>
               <img src={stocks} alt={""} width={"100%"}></img>
+            </div>
+          </div>
+          <div className={"single_project"}>
+            <div className={"projects_text"}>
+              <h4 className={"project_item"}>
+                Grace In Space
+                <a href="https://github.com/graceinspace/graceInSpace">
+                  <img
+                    className={"github_space"}
+                    src={github}
+                    width={30}
+                    alt={""}
+                  ></img>
+                </a>
+              </h4>
+              <h5 className={"project_description"}>
+                Grace in Space is a virtual reality mobile game in which players
+                help an elderly alien collect items that flew out of her bag
+                while she was traveling through space on vacation. <br />
+                <br />
+                It was built with Node.js, JavaScript, React Native, Redux, Viro
+                React, Firebase, Firestore, and Git
+                <br />
+              </h5>
+            </div>
+            <div className={"demos"}>
+              <Embed
+                className={"project_item"}
+                url={
+                  "https://www.youtube.com/watch?v=Tg7BLim7ues&list=PLx0iOsdUOUmnZXS2H9EDbhYMlmh3onWHs&index=6&t=0s&fbclid=IwAR2kzuxHrUMe7-RLOt7l7hPCf-cFQ_p8tWf3wwxrgjEzDnx_NX33A5o_Fb4"
+                }
+              />
+              <br />
+              <Embed
+                url={
+                  "https://www.youtube.com/watch?v=-HUuZn3BKrQ&feature=youtu.be"
+                }
+              />
             </div>
           </div>
           <div className={"single_project"}>
